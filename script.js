2e2d775513f83eb50c3099ce4d48ee4c3ccbabdc
张欧文 <1534661514@qq.com>
2023-03-21 12:01:42 +0800

    update main page

--- script.js
+++ script.js
@@ -8,3 +8,17 @@
 //     nav.classList.remove("active");
 //   }
 // }
+
+const start = document.querySelector(".start");
+const start2 = document.querySelector(".start2");
+const hero = document.querySelector(".container1");
+const main = document.querySelector(".container2");
+
+start.addEventListener("click", () => {
+  hero.classList.toggle("hide");
+  main.classList.toggle("hide");
+});
+start2.addEventListener("click", () => {
+  hero.classList.toggle("hide");
+  main.classList.toggle("hide");
+});
